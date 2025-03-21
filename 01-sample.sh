67d26783d7081783bf44a09df17fd9f4dedc4356
Sathish <sathishu269@gmail.com>
2025-03-21 23:37:16 +0530

    Printing colours

---
+++ 01-sample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Shebang notation: This is tells the script with which interpretor this script should run
+# Shebang notation always in the first line of the script and there can be only one interpretor for the script
+
+echo welcome to Bash scripting
+
+# This is how we will execute the script:
+ # bash scriptname.sh  (or) sh scriptname.sh (or) ./scriptname.sh
+
+echo line1
+echo line2
---
+++ 03-colors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Each and every colour which we see on a terminal will have a colour code and we need to use the code based on our need.
+
+#Colours     foreground         Background
+#Red           31                41      
+#Green         32                42
+#Yellow        33                43
+#Blue          34                44
+#Magenta       35                45
+#Cyan          36                46
+
+# Syntax to print the colour text is :
+
+# echo -e "\e[COLOURCODEm yoour message to be printes in colour \e[0m"
+
+echo -e "\e[33m I am printing yellow colour \e[0m"
+
