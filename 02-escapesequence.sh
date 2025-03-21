091e7b6a2c59f882ac5c169f1330880c0c21baa9
Sathish <sathishu269@gmail.com>
2025-03-21 22:37:14 +0530

    TabSpace

--- 02-escapesequence.sh
+++ 02-escapesequence.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo -e "Line5\nLine6"
+echo -e "Line1\tLine2"
 
 # e ---> Enables the escape sequence character
 # when ever your using special characters like \n keep the input in double quotes
+
+# \n ----> This indicates the new line
+# \t -----> This indicates the tab space
+
