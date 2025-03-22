8e5654cdcb0e48833b86518ed7a6e4ba6d4692eb
Sathish <sathishu269@gmail.com>
2025-03-22 22:07:27 +0530

    fu

--- 10-commands.sh
+++ 10-commands.sh
@@ -6,4 +6,13 @@
 # 2) Aliases --------------> (Aliases are shorcut alias net="netstat")
 # 3) Shell Built in commands
 # 4) Functions ---> Functions are nothing but a set of commands that can be written in a sequence and called n number of times as per requirement
- 
+
+ # How to declare a Function
+
+ # This is how we can delcare a function 
+ # f() {
+ #echo hai
+ # }
+
+ # This is how we call a function 
+# f
