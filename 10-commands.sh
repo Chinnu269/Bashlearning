6aeeb57406fe064ebae72ca7b30fcedb9b3974cf
Sathish <sathishu269@gmail.com>
2025-03-22 22:02:11 +0530

    F

---
+++ 10-commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# There are 4 types of commands avaiable
+
+# 1) Binaray ------------->  (./bin ..., ./sbin)
+# 2) Aliases --------------> (Aliases are shorcut alias net="netstat")
+# 3) Shell Built in commands
+# 4) Functions ---> Functions are nothing but a set of commands that can be written in a sequence and called n number of times as per requirement
+ 
