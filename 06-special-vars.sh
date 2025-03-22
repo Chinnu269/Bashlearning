fedd6a252bf33f95ce20ba40de2b3e6b90b422d5
Sathish <sathishu269@gmail.com>
2025-03-22 20:46:09 +0530

    Special

---
+++ 06-special-vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Special variables will give some special properties to your variables
+# Few of the special variables are $0 to $9, $?, $#, $*, $@
+
+echo $0   # prints the current script name that your executing
+echo "Executed script name is : $0"
+
+
