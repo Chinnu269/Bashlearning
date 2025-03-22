dfe2ce5d8ad5bb721bb8338d351b1f745eb7dee1
Sathish <sathishu269@gmail.com>
2025-03-22 21:53:51 +0530

    break

--- 09-read.sh
+++ 09-read.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# read is a prompt variable when ever you want to break the script and give the input read will be used
 read -p "Enter your Name:" name
 echo -e "Name of the person who ran the script is $name"
