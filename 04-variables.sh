cbb12096c7fc95e6b08b18a214766c0252558a0f
Sathish <sathishu269@gmail.com>
2025-03-22 11:48:17 +0530

    print static variable

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,6 +6,7 @@
 a=10     #  a is an integer
 b=10     # b is an integer
 c=xyz    # c is a string
+DATE="22March2025"
 
 # There is no concept of data types in shell scripting by default everything is a string
 # If your input or variables haing some special characters always include them in the double quotes
@@ -14,11 +15,11 @@ c=xyz    # c is a string
 # $ : Dollar symbol is the special character is used to print the variables.
 # Syntax:  echo $a
 
-echo a                  # Prints  just a
-echo "print the value of a:$a"                 # Print the vaule of a
-echo "print the vaule of a: ${a}"               # $a and ${a} both are same they both do the same job but prefer to use with in the flower bracket
+echo a                               # Prints  just a
+echo "print the value of a:$a"       # Print the vaule of a
+echo "print the vaule of a: ${a}"    # $a and ${a} both are same they both do the same job but prefer to use with in the flower bracket
 echo "print the vaule of X: ${x}"    # we are not defined any vaule of x but trying to print it gives "Null" it doesn't through any error
-
+echo "Today's date is: ${DATE}"      # Tp print the date,it is a static variable
 
 #[]  ---> Square bracket
 #()  ---> Paranthesis
