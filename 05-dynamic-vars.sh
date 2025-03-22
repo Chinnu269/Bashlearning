48c5082179a7932fbd8d58d37a709a844f2e7847
Sathish <sathishu269@gmail.com>
2025-03-22 12:19:35 +0530

    Sessions

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -2,3 +2,7 @@
 
 DATE="$(date +%D)"      # when ever we are giving input as a dynamic variabe we have to keep it in paranthesis() and giving input as a dynmic variable is called Expression
 echo "Today's Date is ${DATE}"
+
+Sessions_Count= "$(who |wc -l)"
+echo "Number of active sessions are: ${Sessions_Count}"
+
