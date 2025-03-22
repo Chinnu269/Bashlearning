2b7bda54769795ba1dfd0f63dfc6e000b7d759e8
Sathish <sathishu269@gmail.com>
2025-03-22 12:10:00 +0530

    V

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-DATE="$(date+%D)"      # when ever we are giving input as a dynamic variabe we have to keep it in paranthesis() and giving input as a dynmic variable is called Expression
+DATE="$(date +%D)"      # when ever we are giving input as a dynamic variabe we have to keep it in paranthesis() and giving input as a dynmic variable is called Expression
 echo "Today's Date is ${DATE}"
