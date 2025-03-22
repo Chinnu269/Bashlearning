2696a4999145545ef4bd42904f77e42404374e3a
Sathish <sathishu269@gmail.com>
2025-03-22 21:28:54 +0530

    Exit

--- 07-exitcodes.sh
+++ 07-exitcodes.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# Each and every action in the linux will have the exit codes
+# The Range of Exit codes are from 0 to 255
+# Among all '0' represents action completed successfull
+# Anything in between 1 to 255 represet either partial success, partial failure, failure exit codes
+
