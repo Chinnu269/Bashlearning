0e5474c36479b2823473fabd2f83c2b050314593
Sathish <sathishu269@gmail.com>
2025-03-22 20:53:17 +0530

    Vars

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -6,4 +6,10 @@
 echo $0   # prints the current script name that your executing
 echo "Executed script name is : $0"
 
+echo "Name of the top selled cars in india is $1"
+echo "Name of the current topic is $2"
+echo "Who is the PM of India:$3"
+
+# bash scriptname.sh arg1 arg2 arg3  ----> (bash scriptname.sh $1 $2 ....$9)
+
 
