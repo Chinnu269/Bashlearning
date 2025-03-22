ec8575bd50dd3f524ee1bd7a1c4903f228004121
Sathish <sathishu269@gmail.com>
2025-03-22 21:42:41 +0530

    quotes

---
+++ 08-quotes.sh
@@ -0,0 +1,8 @@
+echo '$$'    # It is going to print the PID of the current process
+echo '$#'    # It is going to print the number of arguments we are passing
+echo '$?'    # It is going to print the exit code of the last command 
+echo '$*'    # It is going to print the list of arguments used
+echo '$@'    # It is going to print the list of aguments used
+
+# Single quotes will surppress the power of special variable
+# Whenever you have any special variables. prefer to avoid single quotes and proceed with double quotes
