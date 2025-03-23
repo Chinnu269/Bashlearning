1b3b76cac2430355f867fcfc82eda09e2b8e1c83
Sathish <sathishu269@gmail.com>
2025-03-23 11:55:59 +0530

    If

---
+++ 14-if-else.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# If condition is available in 3 formats
+
+# 1) simple if
+# 2) if else
+# 3) else if
+
+<<COMMENT
+
+1) simple If :
+
+if [ expression ]; then
+   commands
+
+fi
+
+# simple if: Commands are getting executed only if the expression is true
+# What will happen if the expression is false ? simple,commands won't be executed
+
+2) If-else:
+
+if [ expression ]; then
+    command1
+else
+    command2
+
+fi
+
+# If expression is true command 1 will be executed if expression is not true then command 2 will be executed
+
+3) Else-if:
+
+if [ expression1 ]; then
+  command1
+elif [ expression2 ]; then
+  command2
+elif [ expression3 ]; then
+  command3
+
+else
+
+ command4
+
+fi
+
+COMMENT
+
+
+
