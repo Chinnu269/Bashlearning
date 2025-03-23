ba2a9706752c72facfe715ffb4fc45304e4eeb46
Sathish <sathishu269@gmail.com>
2025-03-23 11:19:05 +0530

    cond

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -12,16 +12,20 @@ Action=$1
 case $Action in
     start)
       echo -e "\e[32m starting the payment service \e[0m"
+      exit 1
       ;;
     stop)
      echo -e "\e[31m stoppng the payment service \e[0m"
+     exit 2
       ;;
     restart)
       echo -e "\e[33m restartng the payment service \e[0m"
+      exit 3
       ;;
     *)
       echo -e "\e[34m valid optins are start or stop or restart \e[0m"
       echo -e "\e[35m Exmaple usage \e[0m :\n \t bash scriptname stop"
+      exit 4
       ;;
 esac
 
