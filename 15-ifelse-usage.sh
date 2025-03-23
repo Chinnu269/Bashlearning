fc69f603d4280a8a0768ba1989d9ff9ef14ff9ea
Sathish <sathishu269@gmail.com>
2025-03-23 12:40:10 +0530

    elseif

--- 15-ifelse-usage.sh
+++ 15-ifelse-usage.sh
@@ -2,12 +2,20 @@
 
 
 echo "Demo on If conditions usage"
+
 Action=$1
 if [ "$Action" == "start" ]; then
    echo "Starting Payment Service"
    exit 0
+else if [ "$Action" == "stop" ]; then
+   echo "Stopping Payment Service"
+   exit 1
+else if [ "$Action" == "restart" ]; then
+   echo "restarting the payment Service"
+   exit 2
 else
-  echo "Valid option is start only"
-  exit 1
+   echo -e "\e[34m valid optins are start or stop or restart \e[0m"
+   echo -e "\e[35m Exmaple usage \e[0m :\n \t bash scriptname stop"
+   exit 3
 fi
 
