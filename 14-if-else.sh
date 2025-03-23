133e229ad8475665d7b7f848a06e147443189202
Sathish <sathishu269@gmail.com>
2025-03-23 13:22:26 +0530

    Message

--- 14-if-else.sh
+++ 14-if-else.sh
@@ -68,5 +68,24 @@ Operators on numbers
 
  Eg:  [ abc=abc ]
 
+ -z , -n 
+
+    [ -z "$var" ] -> This is true if var is not having any data
+    [ -n "$var" ] _> This is true if var is having any data
+
+    -z and -n are inverse proportional options
+
+
+Operators on files:
+
+    Lot of operators are available and you can check them using man pages of bash 
+
+    [ -f file ] -> True of file exists and file is a regular file 
+
+    [ -d xyz ]  -> True if file exists and it is a directory
+
+    ### Explore the file types, There are 7 types on files in Linux.
+
+
 COMMENT
 
