4f31aa105f78e617c51ea7d7e16129bca5838775
Sathish <sathishu269@gmail.com>
2025-03-23 10:23:30 +0530

    re

---
+++ 11-redirectors.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Redirectors are 2 types in Bash
+# 1) Input Redireector  (which means takes the I/P from the file) :  <  (Ex: mysql </tmp/studentapp.sql)
+# 2) Output Redirector  (which means routing the O/P to a file)   :  > or 1> or >> (>> appends the latest o/p with the existing content)
