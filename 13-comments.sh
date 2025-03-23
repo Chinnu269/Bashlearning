ec980d65a06f1112e79b515eeaf2d3c6db91208e
Sathish <sathishu269@gmail.com>
2025-03-23 11:27:41 +0530

    com

---
+++ 13-comments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+<<COMMENT
+echo "Cloud and Devops Training"
+echo " learning Bash scripting"
+a=10
+b=20
+echo $a
+COMMENT
+echo $b
+
+# This is an exmple of multiline comments what ever we have enclosed in between <<COMMENT    COMMENT will be considered as comment
+
+
