9186ce5d3f94e0304ba636f6e4fc52e91314a896
Sathish <sathishu269@gmail.com>
2025-03-25 08:02:40 +0530

    ex

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -5,7 +5,7 @@
 USER_ID=$(id -u)
  
 if [ $USER_ID -ne 0 ]; then
-echo -e "\e[31m Script is executed by the root user or sudo privilages \e[0m \n Example: sudo bash frontend"
+echo -e "\e[31m Script should be executed by the root user or sudo privilages \e[0m \n Example: sudo bash frontend"
 exit 1
 fi
 
