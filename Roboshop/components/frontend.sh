ceb3f173cd24cd3acd22e2c015a62ae85edf2819
Sathish <sathishu269@gmail.com>
2025-03-26 11:02:47 +0530

    Arg

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -9,30 +9,26 @@ echo -e "\e[31m Script should be executed by the root user or sudo privilages \e
 exit 1
 fi
 
+stat() {
+if [ $1 -eq 0 ]; then
+  echo -e  "\e[32m success \e[0m"
+else
+  echo -e "\e[31m Failure \e[0m"
+fi
+
+}
+
   echo -e "\e[34m Configuring frontend \e[0m \n"
   echo -n "Installing frontend: "
   dnf install nginx -y  &>> /tmp/frontend.log
+  stat $?
 
-if [ $? -eq 0 ]; then
-  echo -e  "\e[32m success \e[0m"
-else
-  echo -e "\e[31m Failure \e[0m"
-fi
 
- echo -n "starting nginx: "
- systemctl enable nginx  &>> /tmp/frontend.log
- systemctl start nginx   &>> /tmp/frontend.log
-
- if [ $? -eq 0 ]; then
-  echo -e  "\e[32m success \e[0m"
-else
-  echo -e "\e[31m Failure \e[0m"
-fi
+echo -n "starting nginx: "
+systemctl enable nginx  &>> /tmp/frontend.log
+systemctl start nginx   &>> /tmp/frontend.log
+stat $?
 
 echo -n "Downloading the frontend component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"  &>> /tmp/frontend.log
- if [ $? -eq 0 ]; then
-  echo -e  "\e[32m success \e[0m"
-else
-  echo -e "\e[31m Failure \e[0m"
-fi
+stat $?
